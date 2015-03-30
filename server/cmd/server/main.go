4474c3cd3e336317829e096901bb6728e4a26b68
Casey Kuhlman <casey@erisindustries.com>
2015-03-30 14:33:20 +0200

    add @androlo s work from decerver repository

---
+++ server/cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/eris-ltd/decerver/server"
+)
+
+func main() {
+	srvr := server.NewServer("localhost", 3000, 1000, ".")
+	srvr.Start()
+}
---
+++ server/epm_api.go
@@ -0,0 +1,5 @@
+package server
+
+import ()
+
+// Add some kind of epm api to be injected as a martini service.
